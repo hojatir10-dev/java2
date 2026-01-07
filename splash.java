32e7c83b5b303df6a48e53879059d7893252594d
hojatir10-dev <hojatir10@gmail.com>
2026-01-07 05:32:09 -0800

    Create splash.java

---
+++ splash.java
@@ -0,0 +1,28 @@
+package com.example.smartstudent;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+public class SplashActivity extends AppCompatActivity {
+Button btnexit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            SharedPreferences prefs = getSharedPreferences("user", MODE_PRIVATE);
+            boolean isLoggedIn = prefs.getBoolean("login", false);
+
+            if (isLoggedIn) {
+                startActivity(new Intent(this, MainActivity.class));
+            } else {
+                startActivity(new Intent(this, LoginActivity.class));
+            }
+            finish();
+        }, 2000);
+    }
+}
